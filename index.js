e06f48d01782d69169286aee753a22d5c51a2f35
ghofran <gho0ofran.adel14@gmail.com>
2024-03-12 12:48:34 -0700

    Back-End Setup: Initial Folder Structure & Git Repo

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+const { PORT, CORS_ORIGIN } = process.env;
+const allowedOrigins = CORS_ORIGIN.split(",");
+const port = PORT || 5051;
+
+app.use(cors({ origin: allowedOrigins }));
+app.use(express.json());
+
+const inventoryRoutes = require('./routes/inventory-routes');
+const warehouseRoutes = require('./routes/warehouse-routes');
+
+// All routes
+app.use('/warehouses', warehouseRoutes);
+app.use('/inventories', inventoryRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
