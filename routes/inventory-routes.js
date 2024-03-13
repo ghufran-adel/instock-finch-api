120e5df02f431d8fd5f01a1e40f12aad6a5e5723
Celine Liu <mangolzj@gmail.com>
2024-03-13 13:03:27 -0400

    start on the get inventory item api

--- routes/inventory-routes.js
+++ routes/inventory-routes.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const knex = require("knex")(require("../knexfile"));
 
-
+//API to GET a Single Inventory Item
+router.get("/:id", (req, res) => {
+  // check if id exists in database
+  const inventoryItems = [];
+  if (!req.params || !inventoryItems.includes(req.params)) {
+    res.send("invalid id input"), 404;
+  }
+});
 module.exports = router;
+
+// GET /api/inventories/:id
+
+// Response returns 404 if the ID is not found
+
+// Response returns 200 if successful
+
+// /api/inventories/1 Response body example:
