4af8c51573ad573f1aace70d1a66e56084850b97
Celine Liu <mangolzj@gmail.com>
2024-03-14 12:02:18 -0400

    Created endpoint that will allow users to send a GET request for a warehouse matching the param id

--- controllers/warehouse-controllers.js
+++ controllers/warehouse-controllers.js
@@ -1,16 +1,35 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 // GET warehouse list
 const getWarehouseList = async (_req, res) => {
-    try {
-        const data = await knex('warehouses');
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).send(`Error retrieving Warehouses: ${error}`)
+  try {
+    const data = await knex("warehouses");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).send(`Error retrieving Warehouses: ${error}`);
+  }
+};
+
+const getWarehouseByID = async (req, res) => {
+  try {
+    const warehouseFound = await knex("warehouses").where({
+      id: req.params.id,
+    });
+    console.log(req.params.id);
+    if (!req.params.id || warehouseFound.length === 0) {
+      return res.status(404).json({
+        message: `Inventory with ID ${req.params.id} not found`,
+      });
     }
-}
-
+    const warehousesData = warehouseFound[0];
+    res.json(warehousesData);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to retrieve inventory data for warehouse item with ID ${req.params.id}, encountering error ${error}`,
+    });
+  }
+};
 module.exports = {
-    getWarehouseList,
-
-}
+  getWarehouseList,
+  getWarehouseByID,
+};
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const warehouseController = require('../controllers/warehouse-controllers')
+const warehouseController = require("../controllers/warehouse-controllers");
 
-router
-    .route('/')
-    .get(warehouseController.getWarehouseList);
+router.route("/").get(warehouseController.getWarehouseList);
+router.route("/:id").get(warehouseController.getWarehouseByID);
 
 module.exports = router;
