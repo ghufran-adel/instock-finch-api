95266e396968b032095294a9dda2a9a74b94ae0c
adams-infinity <lindsayadamsdesign@gmail.com>
2024-03-14 12:10:19 -0400

    add postNewWarehouse in ware-house-controllers

--- controllers/warehouse-controllers.js
+++ controllers/warehouse-controllers.js
@@ -10,7 +10,28 @@ const getWarehouseList = async (_req, res) => {
     }
 }
 
+const postNewWarehouse = async (req, res) => {
+    try {
+        const { warehouse_name, address, city, country, contact_name, contact_position, contact_phone, contact_email } = req.body;
+        const newWarehouse = await knex('warehouses').insert({
+            warehouse_name,
+            address,
+            city,
+            country,
+            contact_name,
+            contact_position,
+            contact_phone,
+            contact_email
+        });
+  
+        res.status(201).json(newWarehouse);
+    } catch (error) {
+        res.status(400).json({ error: `Unable to create new warehouse: ${error}` });
+    }
+};
+
 module.exports = {
     getWarehouseList,
+    postNewWarehouse,
 
 }
--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -5,5 +5,6 @@ const warehouseController = require('../controllers/warehouse-controllers')
 router
     .route('/')
     .get(warehouseController.getWarehouseList);
+    
 
 module.exports = router;
