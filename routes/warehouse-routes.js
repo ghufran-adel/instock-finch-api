560a2ba51b2691033de15c7e5fed3ee8a32903c1
adams-infinity <lindsayadamsdesign@gmail.com>
2024-03-15 14:06:29 -0400

    add post router to warehouse-routes

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const warehouseController = require("../controllers/warehouse-controllers");
 
 router
-    .route('/')
+    .route("/")
     .get(warehouseController.getWarehouseList)
     .post(warehouseController.postNewWarehouse);
 
 router
-    .route("/:id").get(warehouseController.getWarehouseByID);
+    .route("/:id")
+    .get(warehouseController.getWarehouseByID);
 
-
-
+router
+    .route('/:id')
+    .patch(warehouseController.updateWarehouse);
 
 module.exports = router;
+
+
+
+
+
+
+
